3f8a668ba5693b64c7b6eef06ee84ec6d9921c3b
mszubert <michal.szub@gmail.com>
2023-06-10 20:49:46 +0200

    war instead of jar

--- src/main/java/com/example/welcomeapp/WelcomeappApplication.java
+++ src/main/java/com/example/welcomeapp/WelcomeappApplication.java
@@ -2,9 +2,10 @@ package com.example.welcomeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WelcomeappApplication {
+public class WelcomeappApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WelcomeappApplication.class, args);
